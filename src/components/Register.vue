5f3fae814ac8cbac1b45e2a65d83a107eec5fc11
ChinonsoIke <chinonsoikewelugo@gmail.com>
2021-12-12 13:27:45 +0100

    edit login form width

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-blue-600 p-5 pt-20 w-full grid justify-items-center h-auto">
+    <div class="bg-blue-600 xs:px-2 xs:py-5 sm:px-5 sm:py-5 pt-20 w-full grid justify-items-center h-auto">
         <div class="grid justify-items-center">
             <p class="text-white text-3xl font-bold mb-1">So, this is your first time?</p>
             <p class="text-white xs:text-sm md:text-lg mb-5">Don't be shy. Sign up on Nkata to chat with friends</p>
